db1b667d39d4c409eeac8c7e00d855951405d5a3
Scott Lee <scottlee@Scotts-MacBook-Pro-3.local>
2020-11-08 21:02:00 -0800

    minor changes

---
+++ add_to_playlists.py
@@ -0,0 +1,17 @@
+import requests
+
+class AddToPlaylists:
+
+    def __init__(self, user, token):
+        self.spotify_user_id = user
+        self.spotify_token = token
+
+    def get_song(self, artist, song_name):
+        pass
+
+    def get_playlist(self, playlist_name):
+        pass
+
+    def add_song_to_playlists(self):
+        pass
+
