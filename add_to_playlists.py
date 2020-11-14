246d7166e88ec07071af13a277422fb5a59e318f
Scott Lee <scottlee@Scotts-MacBook-Pro-3.local>
2020-11-14 09:27:22 -0800

    add song to playlists

--- add_to_playlists.py
+++ add_to_playlists.py
@@ -1,4 +1,7 @@
 import requests
+import json
+
+from exceptions import ResponseException
 
 class AddToPlaylists:
 
@@ -67,8 +70,40 @@ class AddToPlaylists:
         return None
 
 
+    def add_song_to_playlist(self, playlist_id, song_id):
+        songs = [song_id]
+        request_data = json.dumps(songs)
+        
+        query = "https://api.spotify.com/v1/playlists/{}/tracks".format(
+            playlist_id)
+
+        response = requests.post(
+            query,
+            data = request_data,
+            headers={
+                "Content-Type": "application/json",
+                "Authorization": "Bearer {}".format(self.spotify_token)
+            }
+        )
+
+        # check for valid response status
+        if response.status_code != 200:
+            raise ResponseException(response.status_code)
+
+        response_json = response.json()
+        return response_json
+        
 
 
-    def add_song_to_playlists(self):
-        pass
+    def add_song_to_playlists(self, string_list, artist, song_name):
+        song_id = self.get_song(artist, song_name) # song id
+
+        # get id list for playlist
+        id_list = []
+        for i in range(len(string_list)):
+            id_list[i] = self.get_playlist(string_list[i])
+
+        # add song to playlists
+        for i in range(len(id_list)):
+            self.add_song_to_playlist(id_list[i], song_id)
 
